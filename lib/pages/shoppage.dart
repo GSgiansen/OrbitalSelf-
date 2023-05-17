657d015be693e7a86928043177ca81dffbdc9649
Jason Baek <bkjwjason@gmail.com>
2023-05-17 18:39:26 +0800

    fixed nested header

--- lib/pages/shoppage.dart
+++ lib/pages/shoppage.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:orbital_test_space/components/shopcard.dart';
 import 'package:orbital_test_space/components/header.dart';
 import 'package:orbital_test_space/main.dart';
+
 class ShopPage extends StatefulWidget {
-  ShopPage({super.key, required this.title, required CurrencyNotifier this.currencyNotifier, required this.itemsOwned});
+  ShopPage(
+      {super.key,
+      required this.title,
+      required CurrencyNotifier this.currencyNotifier,
+      required this.itemsOwned});
   final String title;
   final CurrencyNotifier currencyNotifier;
   final ItemsOwned itemsOwned;
@@ -16,12 +21,13 @@ class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: header(context, widget.currencyNotifier, false),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ShopCard(currencyNotifier: widget.currencyNotifier, itemsOwned: widget.itemsOwned),
+            ShopCard(
+                currencyNotifier: widget.currencyNotifier,
+                itemsOwned: widget.itemsOwned),
             const Spacer(),
             ElevatedButton(
               onPressed: () {
