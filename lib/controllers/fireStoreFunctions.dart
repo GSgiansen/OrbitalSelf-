8b40d2c1ebd75b6c51599201c26dc79e1f2dfab1
GianSen <giansenrockz@gmail.com>
2023-06-19 00:25:40 +0800

    changed struct of firebase collections for the items

--- lib/controllers/fireStoreFunctions.dart
+++ lib/controllers/fireStoreFunctions.dart
@@ -1,13 +1,10 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../models/item.dart';
 
 class FireStoreFunctions {
-
   static FirebaseFirestore db = FirebaseFirestore.instance;
-
 
   static getCurrentUser(String email) {
     return FirebaseFirestore.instance
@@ -17,53 +14,53 @@ class FireStoreFunctions {
         .then((value) => print("User Retrieved"))
         .catchError((error) => print("Failed to retrieve user: $error"));
   }
+
   static getCurrentUserCurrency({required User? user}) {
     var email = user?.email;
-    
+
     return db
         .collection("users")
         .doc(email)
         .get()
         .then((DocumentSnapshot documentSnapshot) {
-          if (documentSnapshot.exists) {
-            print(documentSnapshot.get("currency"));
-            return documentSnapshot.get("currency");
-          } else {
-            print("0");
-          }
-        })
-        .catchError((error) => print("Failed to retrieve user: $error"));
+      if (documentSnapshot.exists) {
+        print(documentSnapshot.get("currency"));
+        return documentSnapshot.get("currency");
+      } else {
+        print("0");
+      }
+    }).catchError((error) => print("Failed to retrieve user: $error"));
   }
 
-
-  
   static addNewUser(String email, String password) {
     return db
-        .collection("users")
+       .collection("users")
         .doc(email)
         .set({
           "email": email,
           "password": password,
           "currency": 0,
-          "items": List<Item>.empty(growable: true),
+          "items":
+              List<Item>.empty(growable: true), //Change to handle map of map
         })
         .then((value) => print("User Added to Firestore"))
         .catchError((error) => print("Failed to add user: $error"));
   }
 
-  static addNewPurchase(User? user, String item) {
+  static addNewPurchase(User? user, String item) async {
     var email = user?.email;
+    
     return db
         .collection("users")
         .doc(email)
-        .set(
+        .update(
           {
-            "items": FieldValue.arrayUnion([item]),
-          },
-          SetOptions(merge: true),
+            "items.${item}.number":FieldValue.increment(1)
+          }
         )
         .then((value) => print("Purchase Added"))
         .catchError((error) => print("Failed to add purchase: $error"));
+        
   }
 
   static addNewCurrency(String email, int currency) {
@@ -72,9 +69,7 @@ class FireStoreFunctions {
         .doc(email)
         .set({
           "currency": currency,
-        },
-        SetOptions(merge: true)
-        )
+        }, SetOptions(merge: true))
         .then((value) => print("Currency Added to Firestore"))
         .catchError((error) => print("Failed to add currency: $error"));
   }
@@ -86,11 +81,8 @@ class FireStoreFunctions {
         .doc(email)
         .set({
           "currency": currency,
-        },
-        SetOptions(merge: true)
-        )
+        }, SetOptions(merge: true))
         .then((value) => print("Currency Removed"))
         .catchError((error) => print("Failed to remove currency: $error"));
   }
-
 }
---
+++ lib/controllers/inventoryToUnity.dart
@@ -0,0 +1,31 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../models/item.dart';
+
+class InventoryToUnity {
+
+  // a method to call to produce the items in the inventory visually for the users
+  List<Item> ItemsAvailableToSpawn(User? user) {
+    List<Item> lst = [];
+    
+
+
+    return lst;
+
+    
+
+  }
+
+
+  // a method to add the inventory item to unity
+
+
+
+
+
+
+
+
+}
