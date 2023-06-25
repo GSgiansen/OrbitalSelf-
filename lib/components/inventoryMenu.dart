27afff9c6be301420a615869ab5e45e80f2b6e6c
tan  gian sen <tangiansen@tans-mbp.lan>
2023-06-25 17:55:56 +0800

    Modified bug where spawning item does not remove from inventory

--- lib/components/inventoryMenu.dart
+++ lib/components/inventoryMenu.dart
@@ -29,75 +29,74 @@ class _InventoryMenuState extends State<InventoryMenu> {
       .snapshots();
   @override
   Widget build(BuildContext context) {
-    List<Widget> lst = [];
     return StreamBuilder<QuerySnapshot>(
-      stream: _usersStream,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text('Something went wrong');
-        }
+  stream: _usersStream,
+  builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+    if (snapshot.hasError) {
+      return Text('Something went wrong');
+    }
 
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
-        }
-        return ListView(
-          children: snapshot.data!.docs.map((DocumentSnapshot document) {
-            Map<String, dynamic> data =
-                document.data()! as Map<String, dynamic>;
-            if (data.containsKey('items')) {
-              Map<String, dynamic> items =
-                  data['items'] as Map<String, dynamic>;
-              //print(items);
-              for (var item in items.entries) {
-                Map<String, dynamic> indiv = item.value;
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return Text("Loading");
+    }
+    
+    List<Widget> lst = []; // Declare the list here
+    
+    snapshot.data!.docs.forEach((DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+      if (data.containsKey('items')) {
+        Map<String, dynamic> items = data['items'] as Map<String, dynamic>;
+        
+        for (var item in items.entries) {
+          Map<String, dynamic> indiv = item.value;
 
-                lst.add(ListTile(
-                  title: Text(
-                    indiv["name"],
+          lst.add(ListTile(
+            title: Text(
+              indiv["name"],
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+                fontFamily: 'Rotorcap',
+              ),
+            ),
+            subtitle: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Text(
+                    "X " + indiv["number"].toString(),
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 24,
                       fontFamily: 'Rotorcap',
                     ),
                   ),
-                  subtitle: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-
-                    children: [
-                      Expanded(
-                        child: Text(
-                          "X " + indiv["number"].toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24,
-                            fontFamily: 'Rotorcap',
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: IconButton(
-                          icon: Icon(Icons.add),
-                          onPressed: () {
-                            print("spawn object done");
-                            String objectname = indiv["name"].toLowerCase();
-                            print("object is" + objectname);
-                            loadChosenObject(widget.unityWidgetController, objectname);
-                            print("object is" + objectname);
-                          },
-                        ),
-                      )
-                    ],
+                ),
+                Expanded(
+                  child: IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      print("spawn object done");
+                      String objectname = indiv["name"].toLowerCase();
+                      loadChosenObject(widget.unityWidgetController, objectname);
+                      print("object is" + objectname);
+                      FireStoreFunctions.removeOldPurchase(FirebaseAuth.instance.currentUser, objectname);
+                    },
                   ),
-                ));
-              }
-            }
-            return Column(
-              children: lst,
-            );
-          }).toList(),
-        );
-      },
+                )
+              ],
+            ),
+          ));
+        }
+      }
+    });
+    
+    return ListView(
+      children: lst,
     );
+  },
+);
+
   }
 }
