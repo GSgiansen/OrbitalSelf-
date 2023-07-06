75d0fa251c99f5bd6c7192e91423c8cd41b18474
Jason Baek <bkjwjason@gmail.com>
2023-07-06 23:03:50 +0800

    add web/ to assets under pubspec.yaml

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,7 +9,6 @@ class MyLoginPage extends StatefulWidget {
 }
 
 class _MyLoginPageState extends State<MyLoginPage> {
-
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
@@ -19,55 +18,52 @@ class _MyLoginPageState extends State<MyLoginPage> {
         body: ListView(children: <Widget>[
       Container(
         decoration: const BoxDecoration(
-            image: DecorationImage(image: AssetImage('web/forest_login.jpg'))
+          image: DecorationImage(image: AssetImage('web/forest_login.jpg'))
             ),
-
-      child: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-            const SizedBox(height: 90.0),
-            const Text('SELF++',
-                style: TextStyle(
-                    fontFamily: 'Rotorcap', fontSize: 95, color: Colors.white)),
-            const SizedBox(height: 10.0),
-            RichText(
-                textAlign: TextAlign.center,
-                text: const TextSpan(
+        child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+              const SizedBox(height: 90.0),
+              const Text('SELF++',
                   style: TextStyle(
                       fontFamily: 'Rotorcap',
-                      fontSize: 22,
-                      color: Color(0xFFd8ead7)),
-                  children: [
-                    TextSpan(text: 'EMBARK ON YOUR JOURNEY TO\n'),
-                    WidgetSpan(child: SizedBox(height: 25)),
-                    TextSpan(text: 'LEVEL UP YOUR LIFESTYLE.')
-                  ],
-                )),
-            const SizedBox(height: 180),
-            RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
+                      fontSize: 95,
+                      color: Colors.white)),
+              const SizedBox(height: 10.0),
+              RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(
                     style: TextStyle(
-                        fontFamily: 'Rotorcap', 
-                        color: const Color(0xFF5c5a71), 
-                        fontSize: 0.08 * height),
+                        fontFamily: 'Rotorcap',
+                        fontSize: 22,
+                        color: Color(0xFFd8ead7)),
                     children: [
-                      //make the text responsive to screen size
-                      TextSpan(
-                          text: 'LOGIN\n',
-                          style: TextStyle(fontSize: 0.05 * height)),
-                      TextSpan(
-                          text: 'TO CONTINUE',
-                          style: TextStyle(fontSize: 0.03 * height))
-                    ])),
-            const SizedBox(height: 40),
-            
-          ]
-          )
-      ),
-          )
-    ])
-    );
+                      TextSpan(text: 'EMBARK ON YOUR JOURNEY TO\n'),
+                      WidgetSpan(child: SizedBox(height: 25)),
+                      TextSpan(text: 'LEVEL UP YOUR LIFESTYLE.')
+                    ],
+                  )),
+              const SizedBox(height: 180),
+              RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                      style: TextStyle(
+                          fontFamily: 'Rotorcap',
+                          color: const Color(0xFF5c5a71),
+                          fontSize: 0.08 * height),
+                      children: [
+                        //make the text responsive to screen size
+                        TextSpan(
+                            text: 'LOGIN\n',
+                            style: TextStyle(fontSize: 0.05 * height)),
+                        TextSpan(
+                            text: 'TO CONTINUE',
+                            style: TextStyle(fontSize: 0.03 * height))
+                      ])),
+              const SizedBox(height: 40),
+            ])),
+      )
+    ]));
   }
 }
